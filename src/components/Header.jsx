745e9fe8647f7d617584a4ea13c1161a4ffc9138
Enpeu Team <info@enpeu.org>
2025-11-14 11:44:37 +0100

    Actualización: logo y foto del equipo desde Fotos/, header estilo página oficial con fondo blanco

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,16 @@ import './Header.css'
 function Header() {
   return (
     <header className="header">
-      <div className="container">
-        <div className="header-content">
-          <Link to="/" className="logo">
-            <img src="/logo-enpeu.png" alt="Enpeu" className="logo-image" />
-          </Link>
-          <nav className="nav">
-            <Link to="/" className="nav-link">Inicio</Link>
-            <Link to="/proyectos" className="nav-link">Proyectos</Link>
-            <Link to="/galeria" className="nav-link">Galería</Link>
-            <Link to="/colaboraciones" className="nav-link">Colaboraciones</Link>
-            <Link to="/blog" className="nav-link">Blog</Link>
-            <Link to="/contacto" className="nav-link">Contacto</Link>
-          </nav>
-        </div>
+      <div className="header-content">
+        <Link to="/" className="logo">
+          <img src="/logo-enpeu.png" alt="Enpeu" className="logo-image" />
+        </Link>
+        <nav className="nav">
+          <Link to="/proyectos" className="nav-link">Proyectos</Link>
+          <Link to="/galeria" className="nav-link">Galería</Link>
+          <Link to="/colaboraciones" className="nav-link">Colaboraciones</Link>
+          <Link to="/contacto" className="nav-link">Contacto</Link>
+        </nav>
       </div>
     </header>
   )
